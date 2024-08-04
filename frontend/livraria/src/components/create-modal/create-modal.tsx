e1a0a9583f13b86373db9e8e3c61bfa0f561e27c
Gabriel Mallaguetti <62891640+Mallaguetti@users.noreply.github.com>
2024-08-04 10:51:19 -0300

    .

--- frontend/livraria/src/components/create-modal/create-modal.tsx
+++ frontend/livraria/src/components/create-modal/create-modal.tsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useBookDataMutate } from "../../hooks/useBookDataMutate";
 import { bookData } from "../../interface/bookData";
+
+import "./create-modal.css";
+
 interface InputProps{
     label: string,
     value: string | number,
     updateValue(value: any): void
+}
+interface ModalProps{
+    closeModal(): void
 }
 
 const Input = ({label, value, updateValue}: InputProps) => {
@@ -16,11 +22,11 @@ const Input = ({label, value, updateValue}: InputProps) => {
     )
 }
 
-export function CreateModal(){
+export function CreateModal({ closeModal }: ModalProps){
     const [title, setTitle] = useState("");
     const [autor, setAutor] = useState("");
     //const [image, setImage] = useState("");
-    const { mutate } = useBookDataMutate();
+    const { mutate, isSuccess, isLoading } = useBookDataMutate();
 
     const submit = () => {
         const bookData: bookData ={
@@ -32,15 +38,22 @@ export function CreateModal(){
         mutate(bookData)
     }
 
+    useEffect(() => {
+        if(!isSuccess) return
+        closeModal
+    }, [isSuccess])
+
     return(
-        <div className="modal-overflow">
+        <div className="modal-overlay">
             <div className="modal-body">
                 <h2>Cadastre um novo item no cardapio</h2>
                 <form className="input-container">
                     <Input label="title" value={title} updateValue={setTitle}/>
                     <Input label="autor" value={autor} updateValue={setAutor}/>
                 </form>
-                <button onClick={submit} className="btn-secondary">Postar</button>
+                <button onClick={submit} className="btn-secondary">
+                    {isLoading ? 'Postando...' : 'Postar'}
+                </button>
             </div>
         </div>
     )//Input label="image" value={image} updateValue={setImage}/> //formatação do btn-secondary aos 33:10 minutos
